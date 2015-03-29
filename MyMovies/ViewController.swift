02ecef1f71cf2fa2ae62f09f40f8115917a5847d
Mario Weidler <mariotrialer@gmail.com>
2015-03-29 18:17:48 +0200

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Mario Weidler on 29.03.15.
+//  Copyright (c) 2015 Mario Weidler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
